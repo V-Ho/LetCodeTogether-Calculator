26e69ebc01c6a796cda0402c95ab64c951233bd8
Vanessa Ho <vanessa.lg.ho@gmail.com>
2019-03-27 18:20:34 +0100

    create math functions

---
+++ calculator.js
@@ -0,0 +1,16 @@
+
+const add = (x, y) => {
+  return x + y
+}
+
+const subtract = (x, y) => {
+  return x - y
+}
+
+const multiply = (x, y) => {
+  return x * y
+}
+
+const divide = (x, y) => {
+  return x / y
+}
