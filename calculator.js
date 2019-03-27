265592c621c2a04f9b4650564fa4c70f41f60aac
Vanessa Ho <vanessa.lg.ho@gmail.com>
2019-03-27 18:21:21 +0100

    basic calculator functions

--- calculator.js
+++ calculator.js
@@ -14,3 +14,52 @@ const multiply = (x, y) => {
 const divide = (x, y) => {
   return x / y
 }
+
+const displayVal = document.getElementById('displayVal')
+
+let valOne
+let valTwo
+let symbol
+let calcArray = []
+
+const emptyCalcArray = () => {
+  calcArray.length = 0
+}
+
+const operatorClick = document.getElementsByClassName('btnOperator')
+for (let arg of operatorClick) {
+  arg.addEventListener('click', () => {
+    symbol = arg.innerHTML
+  })
+}
+
+const numClick = document.getElementsByClassName('btnNum')
+for (let arg of numClick) {
+  arg.addEventListener('click', () => {
+    displayVal.innerHTML = arg.innerHTML
+    calcArray.push(arg.innerHTML)
+    console.log('ARRAY', calcArray)
+    valOne = calcArray[0]
+    valTwo = calcArray[1]
+  })
+}
+
+const evaluate = document.querySelector('#itemg')
+evaluate.onclick = () => {
+  const operators = {
+    '+': add,
+    '-': subtract,
+    'x': multiply,
+    '÷': divide
+  }
+
+  const res = operators[symbol](valOne, valTwo)
+
+  displayVal.textContent = res
+  emptyCalcArray()
+}
+
+const clearBtn = document.querySelector('#itema')
+clearBtn.onclick = () => {
+  displayVal.innerHTML = '0'
+}
